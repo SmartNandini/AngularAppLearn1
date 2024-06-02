69f437adedd44c5c52b37a505a86b21cd76a0783
SmartNandini <nandinijha2k3@gmail.com>
2024-06-03 02:12:47 +0530

    Routing using app.routes.ts!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,13 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ComponentName1Component } from './component-name1/component-name1.component';
 
 export const routes: Routes = [
-    
+    //{'path': '', component: AppComponent},
+    {'path': 'about', component: AboutComponent},
+    {'path': 'contact', component: ContactComponent},
+    {'path': 'comp1', component: ComponentName1Component},
 ];
