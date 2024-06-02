b2b72286f0895ac0848efa7ededfc46468af81dd
SmartNandini <nandinijha2k3@gmail.com>
2024-06-02 23:32:39 +0530

    routing (selectors) in default AppComponent htmp page. Added contact, random and random1 components!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ComponentName1Component } from './component-name1/component-name1.component';
+import { ContactComponent } from './contact/contact.component';
 //import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   //imports: [RouterOutlet, FormsModule,CommonModule],
-  imports: [RouterOutlet, FormsModule, ComponentName1Component],
+  imports: [RouterOutlet, FormsModule, ComponentName1Component, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/component-name1/component-name1.component.ts
+++ src/app/component-name1/component-name1.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+//import { CommonModule } from '@angular/common';
+//import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-component-name1',
   standalone: true,
+  //imports: [CommonModule, FormsModule],
   imports: [],
   templateUrl: './component-name1.component.html',
   styleUrl: './component-name1.component.scss'
 })
 export class ComponentName1Component {
-
+  //hobby: string = '';
 }
