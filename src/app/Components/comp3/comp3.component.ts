b0f77c81de7dc186abfd5d48f9ceb05ff553fc55
SmartNandini <nandinijha2k3@gmail.com>
2024-06-04 15:51:37 +0530

    router redirect used in about page and trying to use routerLinkActive in about and comp3 component newly created!

---
+++ src/app/Components/comp3/comp3.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-comp3',
+  standalone: true,
+  imports: [RouterLink,RouterLinkActive],
+  templateUrl: './comp3.component.html',
+  styleUrl: './comp3.component.scss'
+})
+export class Comp3Component {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,13 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './Components/about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ComponentName1Component } from './component-name1/component-name1.component';
+import { Comp3Component } from './Components/comp3/comp3.component';
 
 export const routes: Routes = [
     //{'path': '', component: AppComponent},
     {'path': 'about', component: AboutComponent},
-    {'path': 'contact', component: ContactComponent},
-    {'path': 'comp1', component: ComponentName1Component},
+    {path: 'contact', component: ContactComponent},
+    {path: 'comp1', component: ComponentName1Component},
+    {path:'comp1Redirect', redirectTo:'comp1'},
+    {path: 'comp3', component: Comp3Component},
 ];
